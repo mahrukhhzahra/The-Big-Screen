db3c2767533fe96b6e817d0a74f807462f393d51
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-12 02:40:04 -0800

    include local database classes [Data class, Dao, Database]

---
+++ app/src/main/java/com/example/thebigscreen/data/local/WatchListDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.thebigscreen.data.local
+
+import androidx.room.AutoMigration
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@AutoMigration(from = 1, to = 2)
+@Database(version = 1, entities = [MyListMovie::class], exportSchema = false)
+abstract class WatchListDatabase: RoomDatabase() {
+    abstract val movieDao: MoviesDao
+}
