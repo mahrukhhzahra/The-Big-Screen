4f7190a092217d0a30b1b788184419c8b8b79d4b
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-13 07:23:23 -0800

    implemented required paging classes

---
+++ app/src/main/java/com/example/thebigscreen/paging/SearchFilmSource.kt
@@ -0,0 +1,44 @@
+package com.example.thebigscreen.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.thebigscreen.data.remote.ApiService
+import com.example.thebigscreen.model.Film
+import com.example.thebigscreen.model.Search
+import com.example.thebigscreen.util.FilmType
+import retrofit2.HttpException
+import java.io.IOException
+
+class SearchFilmSource(
+    private val api: ApiService,
+    private val searchParams: String,
+    private val includeAdult: Boolean
+) :
+    PagingSource<Int, Search>() {
+    override fun getRefreshKey(state: PagingState<Int, Search>): Int? {
+        return state.anchorPosition
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Search> {
+        return try {
+            val nextPage = params.key ?: 1
+            val searchMovies = api.multiSearch(
+                page = nextPage,
+                searchParams = searchParams,
+                includeAdult = includeAdult
+            )
+
+            LoadResult.Page(
+                data = searchMovies.results,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (searchMovies.results.isEmpty()) null else searchMovies.page + 1
+            )
+        } catch (e: IOException) {
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+
+
+}
