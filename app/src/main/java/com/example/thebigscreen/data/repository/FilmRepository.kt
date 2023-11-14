3aaa1e212658037b292672a6d067187ba1600bae
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-14 06:10:11 -0800

    added Resource sealed class for Error and Success

--- app/src/main/java/com/example/thebigscreen/data/repository/FilmRepository.kt
+++ app/src/main/java/com/example/thebigscreen/data/repository/FilmRepository.kt
@@ -1,11 +1,11 @@
 package com.example.thebigscreen.data.repository
 
-import android.content.res.Resources
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.thebigscreen.data.remote.ApiService
 import com.example.thebigscreen.data.remote.response.CastResponse
+import com.example.thebigscreen.data.remote.response.WatchProviderResponse
 import com.example.thebigscreen.model.Film
 import com.example.thebigscreen.paging.BackInTheDaysFilmSource
 import com.example.thebigscreen.paging.PopularFilmSource
@@ -15,7 +15,9 @@ import com.example.thebigscreen.paging.TopRatedFilmSource
 import com.example.thebigscreen.paging.TrendingFilmSource
 import com.example.thebigscreen.paging.UpcomingFilmSource
 import com.example.thebigscreen.util.FilmType
+import com.example.thebigscreen.util.Resource
 import kotlinx.coroutines.flow.Flow
+import timber.log.Timber
 import javax.inject.Inject
 
 class FilmRepository @Inject constructor(private val api: ApiService) {
@@ -40,7 +42,7 @@ class FilmRepository @Inject constructor(private val api: ApiService) {
 
     fun getTopRatedFilms(filmType: FilmType): Flow<PagingData<Film>> {
         return Pager(
-         config = PagingConfig(enablePlaceholders = false, pageSize = 20),
+            config = PagingConfig(enablePlaceholders = false, pageSize = 20),
             pagingSourceFactory = {
                 TopRatedFilmSource(api = api, filmType = filmType)
             }
@@ -81,26 +83,40 @@ class FilmRepository @Inject constructor(private val api: ApiService) {
         return Pager(
             config = PagingConfig(enablePlaceholders = false, pageSize = 20),
             pagingSourceFactory = {
-                RecommendedFilmSource(api = api, filmId = movieId, filmType= filmType)
+                RecommendedFilmSource(api = api, filmId = movieId, filmType = filmType)
             }
         ).flow
     }
 
 
-
-
-
-
     /** Non-paging data **/
 
-    suspend fun getFilmCast(filmId: Int, filmType: FilmType): Resources<CastResponse> {
+    suspend fun getFilmCast(filmId: Int, filmType: FilmType): Resource<CastResponse> {
         val response = try {
-
+            if (filmType == FilmType.MOVIE) api.getMovieCast(filmId = filmId)
+            else api.getTvShowCast(filmId = filmId)
         } catch (e: Exception) {
-            return Resource.
+            return Resource.Error("Error when loading movie cast")
         }
+        return Resource.Success(data = response)
     }
 
+
+    suspend fun getWatchProviders(
+        filmType: FilmType,
+        filmId: Int,
+    ): Resource<WatchProviderResponse> {
+        val response = try {
+            if (filmType == FilmType.MOVIE) api.getWatchProviders(
+                filmPath = "movie", filmId = filmId
+            )
+            else api.getWatchProviders(filmPath = "tv", filmId = filmId)
+        } catch (e: Exception) {
+            return Resource.Error("Error when loading providers")
+        }
+        Timber.d("WATCH", response)
+        return Resource.Success(response)
+    }
 }
 
 
