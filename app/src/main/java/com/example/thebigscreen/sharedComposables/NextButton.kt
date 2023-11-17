f10f5c1eba306570f4c95e0481ede461b201a2c5
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-17 05:36:40 -0800

    Add code for CustomSwitch, MovieGenreChip, NextButton Composables.

---
+++ app/src/main/java/com/example/thebigscreen/sharedComposables/NextButton.kt
@@ -0,0 +1,76 @@
+package com.example.thebigscreen.sharedComposables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.thebigscreen.ui.theme.AppOnPrimaryColor
+import com.example.thebigscreen.ui.theme.ButtonColor
+
+@Composable
+fun NextButton(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        horizontalArrangement = Arrangement.End
+    ) {
+        Box(
+            modifier = Modifier
+                .size(42.dp)
+                .clip(CircleShape)
+                .background(ButtonColor)
+        ) {
+            ConstraintLayout {
+                val icon = createRef()
+                Icon(
+                    imageVector = Icons.Rounded.ArrowForward,
+                    contentDescription = null,
+                    tint = AppOnPrimaryColor,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(6.dp)
+                        .constrainAs(icon) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
+                        .clickable(
+                            interactionSource = remember {
+                                MutableInteractionSource()
+                            },
+                            indication = null
+                        ) { onClick() }
+                )
+
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNexButton() {
+    NextButton {
+
+    }
+}
